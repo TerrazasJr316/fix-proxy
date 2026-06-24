58aa9998217871fe6b35c1999add370ba3ffc863
TerrazasJr316 <terrazasjosue0@gmail.com>
2026-06-24 17:11:07 -0600

    chore(doc): archivos marcados con la solución

---
+++ micro-service/server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const os = require('os');
+const app = express();
+const PORT = 3000;
+
+app.get('/api/v1/saludo', (req, res) => {
+    res.json({
+        message: "hola desde js",
+        // SOLUCIÓN 5: demostración visual; muestra el id que recibió la petición
+        container_id: os.hostname(),
+        date_time: new Date().toISOString()
+    });
+});
+
+app.listen(PORT, () => {
+    console.log(`servidor corriendo en el puerto ${PORT}`);
+});
